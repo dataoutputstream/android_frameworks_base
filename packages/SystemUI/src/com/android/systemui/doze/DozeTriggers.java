f02d292c501b2611f4c4b872ea9ebc62c2d7121d
ezio84 <brabus84@gmail.com>
2020-01-12 18:53:46 +0100

    Flashlight toggle API: extend to screenOn state for Elmyra actions
    Change-Id: Id25d1bbb81caee0c9c362c917df269d78d064340

--- packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
@@ -46,7 +46,6 @@ import com.android.systemui.Dependency;
 import com.android.systemui.R;
 import com.android.systemui.dock.DockManager;
 import com.android.systemui.statusbar.phone.DozeParameters;
-import com.android.systemui.statusbar.policy.FlashlightController;
 import com.android.systemui.util.Assert;
 import com.android.systemui.util.wakelock.WakeLock;
 
@@ -84,7 +83,6 @@ public class DozeTriggers implements DozeMachine.Part {
     private final TriggerReceiver mBroadcastReceiver = new TriggerReceiver();
     private final DockEventListener mDockEventListener = new DockEventListener();
     private final DockManager mDockManager;
-    private final FlashlightController mFlashlightController;
 
     private long mNotificationPulseTime;
     private boolean mPulsePending;
@@ -109,7 +107,6 @@ public class DozeTriggers implements DozeMachine.Part {
                 dozeParameters.getPolicy());
         mUiModeManager = mContext.getSystemService(UiModeManager.class);
         mDockManager = dockManager;
-        mFlashlightController = Dependency.get(FlashlightController.class);
     }
 
     private void onNotification(Runnable onPulseSuppressedListener) {
@@ -356,17 +353,14 @@ public class DozeTriggers implements DozeMachine.Part {
         }
     }
 
-    private void toggleFlashlight() {
+    private void tryToggleFlashlight() {
         proximityCheckThenCall((result) -> {
             if (result == ProximityCheck.RESULT_NEAR) {
                 // in pocket, abort pulse
                 return;
-            } else if (mFlashlightController != null) {
+            } else {
                 // not in pocket, toggle flashlight
-                mFlashlightController.initFlashLight();
-                if (mFlashlightController.hasFlashlight() && mFlashlightController.isAvailable()) {
-                    mFlashlightController.setFlashlight(!mFlashlightController.isEnabled());
-                }
+                mDozeHost.performToggleFlashlight();
             }
         }, false/*performedProxCheck*/, DozeLog.REASON_TOGGLE_FLASHLIGHT);
     }
@@ -603,8 +597,8 @@ public class DozeTriggers implements DozeMachine.Part {
         }
 
         @Override
-        public void toggleCameraFlash() {
-            toggleFlashlight();
+        public void toggleFlashlightProximityCheck() {
+            tryToggleFlashlight();
         }
     };
 }
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -249,6 +249,7 @@ import com.android.systemui.statusbar.policy.ConfigurationController.Configurati
 import com.android.systemui.statusbar.policy.DeviceProvisionedController;
 import com.android.systemui.statusbar.policy.DeviceProvisionedController.DeviceProvisionedListener;
 import com.android.systemui.statusbar.policy.ExtensionController;
+import com.android.systemui.statusbar.policy.FlashlightController;
 import com.android.systemui.statusbar.policy.HeadsUpManager;
 import com.android.systemui.statusbar.policy.KeyguardMonitor;
 import com.android.systemui.statusbar.policy.KeyguardUserSwitcher;
@@ -651,6 +652,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         }
     };
 
+    private FlashlightController mFlashlightController;
     private KeyguardUserSwitcher mKeyguardUserSwitcher;
     protected UserSwitcherController mUserSwitcherController;
     private CurrentUserTracker mUserTracker;
@@ -1229,6 +1231,8 @@ public class StatusBar extends SystemUI implements DemoMode,
 
         // Private API call to make the shadows look better for Recents
         ThreadedRenderer.overrideProperty("ambientRatio", String.valueOf(1.5f));
+
+        mFlashlightController = Dependency.get(FlashlightController.class);
     }
     private void adjustBrightness(int x) {
         mBrightnessChanged = true;
@@ -2361,7 +2365,20 @@ private void drawBlurView() {
 
     @Override
     public void toggleCameraFlash() {
-        mDozeServiceHost.toggleCameraFlash();
+        if (!isScreenFullyOff() && mDeviceInteractive && !mPulsing && !mDozing) {
+            toggleFlashlight();
+            return;
+        }
+        mDozeServiceHost.toggleFlashlightProximityCheck();
+    }
+
+    private void toggleFlashlight() {
+        if (mFlashlightController != null) {
+            mFlashlightController.initFlashLight();
+            if (mFlashlightController.hasFlashlight() && mFlashlightController.isAvailable()) {
+                mFlashlightController.setFlashlight(!mFlashlightController.isEnabled());
+            }
+        }
     }
 
 
@@ -5088,10 +5105,15 @@ private void drawBlurView() {
             return mAnimateScreenOff;
         }
 
-        public void toggleCameraFlash() {
+        public void toggleFlashlightProximityCheck() {
             for (Callback callback : mCallbacks) {
-                callback.toggleCameraFlash();
+                callback.toggleFlashlightProximityCheck();
             }
+        }
+
+        @Override
+        public void performToggleFlashlight() {
+            toggleFlashlight();
         }
     }
 
