5d360e2d7ea281fbb660a024b8648b43f94fdccc
nullxception <enuma.alrizky@gmail.com>
2020-01-26 15:34:48 +0700

    SystemUI: Introduce Adjustable Blur Intensity [1/2]
    Signed-off-by: Reza Adi Pangestu <rezaadipangestu5@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6066,6 +6066,15 @@ public final class Settings {
         private static final Validator QS_BLUR_ALPHA_VALIDATOR = ANY_INTEGER_VALIDATOR;
 
         /**
+         * QS blur alpha
+         * @hide
+         */
+        public static final String QS_BLUR_INTENSITY = "qs_blur_intensity";
+
+        /** @hide */
+        private static final Validator QS_BLUR_INTENSITY_VALIDATOR = ANY_INTEGER_VALIDATOR;
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -6222,6 +6231,7 @@ public final class Settings {
             TEXT_CLOCK_PADDING,
             QS_BLUR
             QS_BLUR_ALPHA
+            QS_BLUR_INTENSITY
         };
 
         /**
@@ -6636,6 +6646,7 @@ public final class Settings {
             VALIDATORS.put(TEXT_CLOCK_PADDING, TEXT_CLOCK_PADDING_VALIDATOR);
             VALIDATORS.put(QS_BLUR, QS_BLUR_VALIDATOR);
             VALIDATORS.put(QS_BLUR_ALPHA, QS_BLUR_ALPHA_VALIDATOR);
+            VALIDATORS.put(QS_BLUR_INTENSITY, QS_BLUR_INTENSITY_VALIDATOR);
         }
 
         /**
