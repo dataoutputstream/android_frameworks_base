093dd35ca87a71e7a88e5ac6f354d34bfd236bac
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-02-04 18:52:22 -0500

    Smartspace: Import Lockscreen weather/live wallpaper [2/2]
    du: ported the code on top of our active edge code
    NB: this must be made here in aosp SystemUI to make it available for all
    devices, not only Soli/Squeeze ones.
    Our google sysui will inherit this code too when expanding aosp sysui classes.
    
    - Includes weather on lockscreen and cool stuff on ambient display
    - Export code to external/Google and leave only bits we need in fwb by Surge1223 <Surge1223@gmail.com>
    
    Change-Id: I47b1a5450a401d5e1678abdff83e5595361bde03

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -265,6 +265,8 @@ import com.android.systemui.tuner.TunerService;
 import com.android.systemui.util.InjectionInflationController;
 import com.android.systemui.volume.VolumeComponent;
 
+import com.google.android.systemui.keyguard.KeyguardSliceProviderGoogle;
+
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -767,7 +769,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         mBubbleController = Dependency.get(BubbleController.class);
         mBubbleController.setExpandListener(mBubbleExpandListener);
         mActivityIntentHelper = new ActivityIntentHelper(mContext);
-        final KeyguardSliceProvider sliceProvider = KeyguardSliceProvider.getAttachedInstance();
+        final KeyguardSliceProvider sliceProvider = KeyguardSliceProviderGoogle.getAttachedInstance();
         if (sliceProvider != null) {
             sliceProvider.initDependencies(mMediaManager, mStatusBarStateController,
                     mKeyguardBypassController, DozeParameters.getInstance(mContext));
@@ -5118,7 +5120,7 @@ private void drawBlurView() {
     }
 
     public boolean isDoubleTapOnMusicTicker(float eventX, float eventY) {
-        final KeyguardSliceProvider sliceProvider = KeyguardSliceProvider.getAttachedInstance();
+        final KeyguardSliceProvider sliceProvider = KeyguardSliceProviderGoogle.getAttachedInstance();
         View trackTitleView = null;
         if (mNotificationPanel != null) {
             trackTitleView = mNotificationPanel.getKeyguardStatusView().getKeyguardSliceView().getTitleView();
